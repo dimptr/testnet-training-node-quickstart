08c59696fe8a776f5d647e75693afa2975148b0b
dimptr <163229212+dimptr@users.noreply.github.com>
2024-10-25 00:02:46 +0800

    Update full_automation.py

--- full_automation.py
+++ full_automation.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
             commit_hash = commit_message.oid
             logger.info(f"Commit hash: {commit_hash}")
             logger.info(f"Repo name: {repo_name}")
-            submit
+            # submit
             submit_task(
                  task_id, repo_name, model2base_model[model_id], gpu_type, commit_hash
             )
