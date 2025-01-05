5b985e0dc874c71aef909f94f05387fd3b689ede
dimptr <163229212+dimptr@users.noreply.github.com>
2025-01-05 12:59:46 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -6,7 +6,10 @@ Yi = {
 }
 qwen_template = {
     "system_format": "<|im_start|>system\n{content}",
-    "user_format": "<|im_start|>user\n{content}"
+    "user_format": "<|im_start|>user\n{content}",
+    "assistant_format": "<|im_start|>assistant\n{content}",
+    "end_format": "
+
 
 
 qwen_template = {
