5abe6fcae5434b3fcfa46aa68bdd562d744b1bb7
dimptr <163229212+dimptr@users.noreply.github.com>
2025-01-06 11:14:10 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -30,6 +30,7 @@ model2template = {
 }
 
 model2size = {
+    "Qwen/Qwen2.5-3B-Instruct":3_000_000_000,
     "Qwen/Qwen1.5-0.5B": 620_000_000,
     "Qwen/Qwen1.5-1.8B": 1_840_000_000,
     "Qwen/Qwen1.5-7B": 7_720_000_000,
